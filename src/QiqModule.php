554fba48236e91160e4d216d6fc0108492e1ab3e
horikawa <horikawa@bitnauts.co.jp>
2022-01-19 13:52:11 +0900

    Split modules

--- src/QiqModule.php
+++ src/QiqModule.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace BEAR\QiqModule;
 
 use BEAR\Resource\RenderInterface;
-use BEAR\Sunday\Extension\Error\ErrorInterface;
 use Ray\Di\AbstractModule;
 use Ray\Di\Scope;
 
@@ -13,6 +12,7 @@ final class QiqModule extends AbstractModule
 {
     public function __construct(
         private string $templateDir,
+        private ?string $errorViewName = null,
         private ?AbstractModule $module = null
     ) {
         parent::__construct($this->module);
@@ -23,8 +23,6 @@ final class QiqModule extends AbstractModule
         $this->bind()->annotatedWith('qiq_template_dir')->toInstance($this->templateDir);
         $this->bind(RenderInterface::class)->to(QiqRenderer::class)->in(Scope::SINGLETON);
 
-        $this->bind(RenderInterface::class)->annotatedWith('error_page')->to(QiqErrorPageRenderer::class);
-        $this->bind(ErrorInterface::class)->to(QiqErrorHandler::class);
-        $this->bind(QiqErrorPage::class);
+        $this->install(new QiqErrorModule($this->errorViewName));
     }
 }
