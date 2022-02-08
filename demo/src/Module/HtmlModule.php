d95c0dba717618d52407a60aa5cc7078653d7ba3
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-02-09 00:48:09 +0900

    Install QiqModule in HtmlModule

---
+++ demo/src/Module/HtmlModule.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyProject\Module;
+
+use BEAR\Dotenv\Dotenv;
+use BEAR\Package\AbstractAppModule;
+use BEAR\Package\PackageModule;
+
+use BEAR\QiqModule\QiqModule;
+use function dirname;
+
+class HtmlModule extends AbstractAppModule
+{
+    protected function configure(): void
+    {
+        $this->install(new QiqModule(dirname(__DIR__, 2) . '/var/template'));
+    }
+}
---
+++ demo/var/template/Index.php
@@ -0,0 +1,1 @@
+Greeting: {{h $this->greeting }}
