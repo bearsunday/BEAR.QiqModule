923881485f940acc8f6aa5a8a14bbeb2291d76c3
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-02-10 19:30:29 +0900

    Add tearDown to recover test file

--- tests/StubGenTest.php
+++ tests/StubGenTest.php
@@ -12,11 +12,16 @@ use function file_get_contents;
 
 class StubGenTest extends TestCase
 {
+    protected function tearDown(): void
+    {
+        copy(__DIR__ . '/Fake/FakeQiq.php.dist', __DIR__ . '/Fake/FakeQiq.php');
+    }
+
     public function testInvoke(): void
     {
         copy(__DIR__ . '/Fake/FakeQiq.php.dist', __DIR__ . '/Fake/FakeQiq.php');
         (new StubGen())(FakeQiq::class, __DIR__ . '/Fake/helper');
-        $fileText =  (string) file_get_contents((string) (new ReflectionClass(FakeQiq::class))->getFileName());
-        $this->assertStringContainsString('@method string foo(string $text)', $fileText);
+        $fileText = (string) file_get_contents((string) (new ReflectionClass(FakeQiq::class))->getFileName());
+        $this->assertStringContainsString('@method string foo(string $fooText)', $fileText);
     }
 }
